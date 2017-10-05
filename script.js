5fc289a0329576c6e4615b17e7c64b0fa0c5034c
Jesse Preiner <jezzah@gmail.com>
2017-10-04 21:49:04 -0600

    Create number of tables based on document width and ideal table width

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 (function() {
+  renderTables();
+  setupCellBehavior();
+})();
+
+
+function renderTables() {
   var numTables = Math.trunc(window.innerWidth/343,10);
-
-  var tableContainer = document.querySelector('.table-container');
-  
+  var tableContainer = document.querySelector('.table-container');    
   var tables = createTables(characters, numTables);
-  tables.forEach(function(table) { console.log(table); tableContainer.appendChild(table)});
+  tables.forEach(function(table) { tableContainer.appendChild(table)});
+}
 
+function setupCellBehavior() {
   var cells = document.getElementsByTagName("td");
   for (var i = 0; i < cells.length; i++ ) {
     cells[i].title = "Click to copy";
@@ -16,37 +22,41 @@
       document.execCommand('copy');
     });
   };
-})();
+}
 
 function createTables(data, numTables) {
-  
-  var table = document.createElement('table');
-  var header = table.createTHead();
-  var columnHeadings = Object.keys(data[0]);
-  var numColumns = columnHeadings.length;
-  var numRows = data.length;
+  var tables = [];
+  var totalRowIndex = 0;
+  for (var tableIndex = 0; tableIndex < numTables; tableIndex++) {
+    var j = 0;
+    var table = document.createElement('table');
+    var header = table.createTHead();
+    var columnHeadings = Object.keys(data[0]);
+    var numColumns = columnHeadings.length;
+    var numRows = data.length;
+    var numRowsPerTable = Math.trunc(numRows/numTables);
+    var row = header.insertRow();
 
-  var row = table.insertRow();
-
-  for (var i = 0; i < numColumns; i++) {
-    var headerCell = document.createElement('th');
-    headerCell.innerText = columnHeadings[i].toUpperCase();
-    row.appendChild(headerCell);
-  }
-
-  var tBody = document.createElement('tbody');
-  table.appendChild(tBody);
-
-  for (var i = 0; i < numRows; i++) {
-    row = tBody.insertRow(-1);
-    for (var j = 0; j < numColumns; j++) {
-      var cell = row.insertCell(-1);
-      cell.setAttribute('data-label', columnHeadings[j].toUpperCase());
-      var obj = data[i];
-      cell.innerText = obj[columnHeadings[j]];
-      row.appendChild(cell);
+    for (colIndex = 0; colIndex < numColumns; colIndex++) {
+      var headerCell = document.createElement('th');
+      headerCell.innerText = columnHeadings[colIndex];
+      row.appendChild(headerCell);
     }
-  }
+    var tBody = document.createElement('tbody');
+    table.appendChild(tBody);
 
-  return Array.from([table]);
+    for (var i = 0; i < numRowsPerTable; i++, totalRowIndex++) {
+      row = tBody.insertRow(-1);
+      for (var j = 0; j < numColumns; j++) {
+        var cell = row.insertCell(-1);
+        cell.setAttribute('data-label', columnHeadings[j]);
+        var obj = data[totalRowIndex];
+        cell.innerText = obj[columnHeadings[j]];
+        row.appendChild(cell);
+      }
+    }
+    tables.push(table);
+  }
+  
+  return Array.from(tables);
 }
